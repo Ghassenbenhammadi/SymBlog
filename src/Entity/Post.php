3f6ef0e18288e0ddadd451af8fbb3973cc724080
Ghassenbenhammadi <benhammdighassen@gmail.com>
2023-09-14 12:52:34 +0200

    initialisation

---
+++ src/Entity/Post.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Cocur\Slugify\Slugify;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+#[UniqueEntity(
+    'slug',
+    message:'Ce slug existe déja'
+)]
+class Post
+{
+
+    const STATES = ['STATE_DRAFT', 'STATE_PUBLIC'];
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique:true)]
+    #[Assert\NotBlank()]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255, unique:true)]
+    #[Assert\NotBlank()]
+    private ?string $slug = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank()]
+    private ?string $content = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $state = Post::STATES[0];
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updated_at = null;
+
+
+    public function __construct()
+    {
+        $this->updated_at = new \DateTimeImmutable();
+        $this->created_at = new \DateTimeImmutable();
+    }
+
+    public function prePersist()
+    {
+        $this->slug = ( new Slugify())->slugify($this->title);
+    }
+    #[ORM\PreUpdate]
+    public function preUpdate()
+    {
+        $this->updated_at = new \DateTimeImmutable();
+    }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updated_at): static
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+    public function __toString()
+    {
+        return $this->title;
+    }
+}
